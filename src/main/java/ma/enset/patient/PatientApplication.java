f99cbcfd4c4a5640bd4349a23f80b494357a1e3e
DRAIA Oussama <100196763+Droronoaa@users.noreply.github.com>
2024-03-17 23:00:29 +0000

    Updating patients template

--- src/main/java/ma/enset/patient/PatientApplication.java
+++ src/main/java/ma/enset/patient/PatientApplication.java
@@ -1,13 +1,27 @@
 package ma.enset.patient;
 
+import ma.enset.patient.entities.Patient;
+import ma.enset.patient.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
 @SpringBootApplication
-public class PatientApplication {
+public class PatientApplication implements CommandLineRunner{
 
+    @Autowired
+    private PatientRepository patientRepository;
     public static void main(String[] args) {
-        SpringApplication.run(PatientApplication.class, args);
+        SpringApplication.run(PatientApplication .class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null, "draia", "oussama", new Date(), true, 10));
+        patientRepository.save(new Patient(null, "ali", "anas",new Date(), false, 20));
+
+
+    }
 }

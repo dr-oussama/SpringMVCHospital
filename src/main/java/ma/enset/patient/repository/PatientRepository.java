9414d71645bb74785294a9785eef6810c26b4c90
DRAIA Oussama <100196763+Droronoaa@users.noreply.github.com>
2024-03-17 22:34:32 +0000

    Creating entity, repository, restController

---
+++ src/main/java/ma/enset/patient/repository/PatientRepository.java
@@ -0,0 +1,14 @@
+package ma.enset.patient.repository;
+
+import ma.enset.patient.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String nom, Pageable pageable);
+}
---
+++ src/main/java/ma/enset/patient/web/PatientRestController.java
@@ -0,0 +1,40 @@
+package ma.enset.patient.web;
+
+import ma.enset.patient.entities.Patient;
+import ma.enset.patient.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class PatientRestController {
+    @Autowired
+    private PatientRepository patientRepository;
+    @GetMapping(path="/index")
+    public String  patients(Model model,
+                            @RequestParam(name="page",defaultValue = "0")int page,
+                            @RequestParam(name="size",defaultValue = "5") int size,
+                            @RequestParam(name="keyword",defaultValue = "") String keyword
+    )
+    {
+        Page<Patient> pagePatients= patientRepository.findByNomContains(keyword, PageRequest.of(page,size));
+        model.addAttribute("listPatients",pagePatients.toList());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",keyword);
+
+        return  "patients"; //return the patients template
+    }
+    @GetMapping(path = "/delete")
+    public String delete(Long id,int page,String keyword ){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+}
